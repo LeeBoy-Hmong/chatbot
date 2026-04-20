d1a81ec10d7470c6a341a9ed6e62ea93368672b2
Michael Lee <michael1998lee@outlook.com>
2026-04-20 12:08:02 -0500

    addition to cache and services

--- backend/ingestion/chunker.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def chunker(documents):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size= 500,
-        chunk_overlap= 100
-    )
-    chunked_docs = text_splitter.split_documents(documents)
-    return chunked_docs
--- backend/ingestion/loader.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader, TextLoader, DirectoryLoader
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Create a function to load the following path for 
