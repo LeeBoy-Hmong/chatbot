65ecd81e1646a5a1706ec6d9abcdcb8e7b241b08
Michael Lee <michael1998lee@outlook.com>
2026-04-20 23:03:10 -0500

    begain loader.py

--- backend/asklee_ai/ingestion/loader.py
+++ backend/asklee_ai/ingestion/loader.py
@@ -1,7 +1,32 @@
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, DirectoryLoader
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Create a function to load the following path for 
+def get_content_wp():
+# Use requests to hit the BetterDocs REST endpoint. The API is paginated, look into 'per_page' and page query 'params' on WP REST.
+    betterdocs = requests.get(os.getenv("BETTERDOCS_REST"))
+    data_content = []
+    current_pg = 1
+# Loop through pages until no results return.
+    while True:
+        parameters = {
+            "page" : current_pg,
+            "per_page" : 100
+        }
+
+        paginated_url = f"{betterdocs}?page={parameters['page']}"
+        print(f"Retrieving: {betterdocs}")
+        # Retrieve 404 error/redirect if page does not exist.
+        if betterdocs.response_status != 200 or len(betterdocs.text) < 500:
+            pass
+# Collect and extract data from the JSON. If there is no data, then stop.
+        data = betterdocs.json()
+        if not data:
+            break
+
+# Strip HTML from 'content.rendered' before returning.
+
+# Return a list of clean dictionaries.
