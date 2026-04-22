de6363a8138fc65d265ff6e7944c6e617f41d8ad
Michael Lee <michael1998lee@outlook.com>
2026-04-22 16:05:51 -0500

    initialized my embeddings

--- backend/asklee_ai/ingestion/pipeline.py
+++ backend/asklee_ai/ingestion/pipeline.py
@@ -1,4 +1,5 @@
 ''' The pipeline is the ochestrator. It'll call my files and push everything into Qdrant'''
+from chunker import chunk_doc
 from qdrant_client import QdrantClient, models
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
@@ -6,11 +7,23 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Initialize Embedding with HuggingFaceEmbeddings - we are using model "multi-qa-MiniLM-L6-cos-v1"
-    # Chosen this model for it's semantic search benchmark.
 client = QdrantClient(
     url=os.getenv("QDRANT_CLUSTER_EP"),
     api_key=os.getenv("QDRANT_API")
 )
-
 print(client.get_collections())
+
+# Initialize Embedding with HuggingFaceEmbeddings - we are using model "multi-qa-MiniLM-L6-cos-v1"
+    # Chosen this model for it's semantic search benchmark.
+m_name = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1"
+m_kwargs = {"device": "cpu"}
+en_kwargs = {"normalize_embeddings": True}  # True, ensures cosine similarity works correctly. Giving more consistent search results especially for semantic RAG systems.
+embeddings = HuggingFaceEmbeddings(
+    model_name=m_name,
+    model_kwargs=m_kwargs,
+    encode_kwargs=en_kwargs
+)
+
+
+
+
