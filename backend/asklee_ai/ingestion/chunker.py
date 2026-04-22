4273dbc480521981997ff2dc32a30b30fb8ce984
Michael Lee <michael1998lee@outlook.com>
2026-04-22 16:06:46 -0500

    Deleted unwanted notes. Too much overhead for production reasons.

--- backend/asklee_ai/ingestion/chunker.py
+++ backend/asklee_ai/ingestion/chunker.py
@@ -1,26 +1,3 @@
-'''The chunkers job is to take the content text and break it into smaller pieces to fit within my embedding model.
-In this case we'll be using 512 token limit from multi-qa-MiniLM-L6-cos-v1 '''
-'''
-### Responsible for splitting documents
-### Should stay dumb and resusable
-### No ML models should be here
-
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-# Recursive chunking - preserves structures and starts with paragraph. Drops to sentences chunking.
-# Semantic coherence is kept, although some coherence may be lost.
-def chunker(documents):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 100,
-        )
-    chunked_docs = text_splitter.split_documents(documents)
-    return chunked_docs
-
-# Create a function with the recursivecharactertextsplitter.
-# Create a variable to assigning to the module function.
-# Utilize the chunk_size and the chunk_overlap parameters.
-# initialize the run with another variable, attache the split_document() method to it.
-'''
 from loader import get_content_wp
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 # Create an instance of RecursiveTextSplitter with basic params to research.
