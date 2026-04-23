487c42e25f0bf067cbac2413532546050abcc079
Michael Lee <michael1998lee@outlook.com>
2026-04-23 16:48:47 -0500

    Upserted properly to Qdrant Cloud, changed to codebase into function for best practice, and confirm code runs properly.

--- backend/asklee_ai/ingestion/pipeline.py
+++ backend/asklee_ai/ingestion/pipeline.py
@@ -10,40 +10,46 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-q_client = QdrantClient(
-    url=os.getenv("QDRANT_CLUSTER_EP"),
-    api_key=os.getenv("QDRANT_API")
-)
+def qdrant_pipeline():
+    q_client = QdrantClient(
+        url=os.getenv("QDRANT_CLUSTER_EP"),
+        api_key=os.getenv("QDRANT_API")
+    )
 
-# Initialize Embedding with HuggingFaceEmbeddings - we are using model "multi-qa-MiniLM-L6-cos-v1"
-    # Chosen this model for it's semantic search benchmark.
-m_name = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1"
-m_kwargs = {"device": "cpu"}
-en_kwargs = {"normalize_embeddings": True}  # True, ensures cosine similarity works correctly. Giving more consistent search results especially for semantic RAG systems.
-embeddings = HuggingFaceEmbeddings(  # Pass this object to your upsert.
-    model_name=m_name,
-    model_kwargs=m_kwargs,
-    encode_kwargs=en_kwargs
-)
+    # Initialize Embedding with HuggingFaceEmbeddings - we are using model "multi-qa-MiniLM-L6-cos-v1"
+        # Chosen this model for it's semantic search benchmark.
+    m_name = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1"
+    m_kwargs = {"device": "cpu"}
+    en_kwargs = {"normalize_embeddings": True}  # True, ensures cosine similarity works correctly. Giving more consistent search results especially for semantic RAG systems.
+    embeddings = HuggingFaceEmbeddings(  # Pass this object to your upsert.
+        model_name=m_name,
+        model_kwargs=m_kwargs,
+        encode_kwargs=en_kwargs
+    )
 
-dimension = len(embeddings.embed_query("testing"))  # Retrieve the dimension without having to guess the size. Pass it through the collection, size parameter.
-print(dimension)
-# Create a collection for the Qdrant vector database.
-collection_doc = "GolieXeeGardens_docs"
-if not q_client.collection_exists(collection_doc):
-    q_client.create_collection(
-        collection_name=collection_doc,
-        vectors_config=VectorParams(size=dimension, distance=Distance.COSINE))
-# Insert the vector into a collection. Utilizing QdrantVectorStore.from_documents()
-# Do not use PointStruct if we are using LangChain.
-lang_doc = chunk_doc(get_content_wp())
-QdrantVectorStore.from_documents(
-    documents=lang_doc,
-    embedding=embeddings,  # Pass the initialized 'embeddings' object created.
-    url=os.getenv("QDRANT_CLUSTER_EP"),
-    api_key=os.getenv("QDRANT_API"),
-    collection_name=collection_doc
-)
+    dimension = len(embeddings.embed_query("testing"))  # Retrieve the dimension without having to guess the size. Pass it through the collection, size parameter.
+    #print(dimension)
+    # Create a collection for the Qdrant vector database.
+    collection_doc = "GolieXeeGardens_docs"  # Created this varidable be the collection was used multiple times.
 
+    if not q_client.collection_exists(collection_doc):
+        q_client.create_collection(
+            collection_name=collection_doc,
+            vectors_config=VectorParams(size=dimension, distance=Distance.COSINE))
+    # Insert the vector into a collection. Utilizing QdrantVectorStore.from_documents()
+    # Do not use PointStruct if we are using LangChain.
+    lang_doc = chunk_doc(get_content_wp())
+    QdrantVectorStore.from_documents(
+        documents=lang_doc,
+        embedding=embeddings,  # Pass the initialized 'embeddings' object created.
+        url=os.getenv("QDRANT_CLUSTER_EP"),
+        api_key=os.getenv("QDRANT_API"),
+        collection_name=collection_doc
+    )
 
+    return(q_client.get_collection(collection_doc))
 
+pipeline = qdrant_pipeline()
+
+if __name__ == "__main__":
+    print(pipeline)
