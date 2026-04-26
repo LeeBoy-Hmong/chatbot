0da532d774cdcb404c4c3e1e86945ab671848a50
Michael Lee <michael1998lee@outlook.com>
2026-04-25 21:03:49 -0500

    Initialized the model with ChatOllama, and created a prompt for instructions with provided parameters on {context} and {question}. Testing model on local laptop.

--- backend/asklee_ai/rag/chain.py
+++ backend/asklee_ai/rag/chain.py
@@ -8,12 +8,29 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
 # Initialize Ollama with ChatOllama
-
+chat_llm = ChatOllama(
+    model="llama3.2:1b",
+    temperature=0.3,  # Increase for creativity, but we need it to be more grounded - keep parameter lower (e.g. 0.2 - 0.3)
+    num_ctx=4096,  # Window size - 4096 is pretty standard.
+    num_predict=512,  # Cut off point at 512 token (approxitmately 384 words)
+    top_p=0.9  # Nucleus Sampling - higher the percentage = higher the probability of most likely words that match.
+)
 # Build the prompt template. Use ChatPromptTemplate.from_template().
+'''  Read me for further instructions...
     # A clear role definition for AskLee
     # {context} placeholder for retrieved chunks. {question} placeholder for user query.
     # An instruction to only answer from context.
+'''
+prompt_template = """
+    You are an AI chatbot name AskLee, an assistant for customers or potential customers for GolieXeeGardens flea market website...
+    Only use the following context to answer...
+    If the answer is not in the context, state "I currently do not have that information"...
+    
+    Context: {context}
+    User Questions: {question}
+"""
 
+chat_prompt = cpt.from_template(prompt_template)
 # Wire the chain with LCEL
 
 # Write a function to ask - takes a question string, invokes the chain, returns the response.
